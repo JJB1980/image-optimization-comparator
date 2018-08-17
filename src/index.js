d183efadc149ad90bbb0f0bd31687f9a24b6015a
john-codeworx <john@codeworx.com.au>
2018-08-17 15:14:09 +1000

    refactoring, testing.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ const {entry} = require('./api');
 if (process.argv[2]) {
   let limit = process.argv[2];
   let preview = process.argv[3] === '-p';
-  let src = process.argv[4];
-  let web = process.argv[5];
+  let sort = process.argv[4];
+  let src = process.argv[5];
+  let web = process.argv[6];
   let terminal = true;
   try {
     if (fs.lstatSync(web).isDirectory() && fs.lstatSync(src).isDirectory() && limit) {
-      entry({src, web, limit, preview, terminal});
+      entry({src, web, limit, sort, preview, terminal});
     }
   } catch (e) {}
 }
